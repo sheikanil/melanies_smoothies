b005428a488629bd0e5fb34c63eb565f8f1a7b9d
sheikanil <36752775+sheikanil@users.noreply.github.com>
2024-06-18 02:22:59 -0500

    Update streamlit_app.py
    Get the Fruityvice Data to Show Data for the Fruits Chosen

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,10 +24,16 @@ ingredients_list = st.multiselect(
     my_dataframe,
     max_selections=5
 )
+
 if ingredients_list:
-    ingredients_string = ' '.join(ingredients_list)
-    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-    fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
+    ingredients_string = ''
+    for fruit_chosen in ingredients_list:
+        ingredients_string += fruit_chosen + ' '
+        fruityvice_response = requests.get(f"https://fruityvice.com/api/fruit/{fruit_chosen}")
+        fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
+        st.image(fv_df['image_url'], caption=fruit_chosen)  # Assuming 'image_url' is a key in the response JSON
+
+    ingredients_string = ingredients_string.strip()  # Remove trailing space
     my_insert_stmt = f"""
         insert into smoothies.public.orders(ingredients, name_on_order)
         values ('{ingredients_string}', '{name_on_order}')
@@ -39,6 +45,6 @@ if ingredients_list:
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success(f"Your Smoothie is ordered! {name_on_order}", icon="✅")
-        
+
 
 
